080ee5467fc1ef57afeca4569e946e15e8072495
Wellington Duarte <44651361+WellingtonWDS@users.noreply.github.com>
2023-08-23 21:00:30 -0300

    fix(): resolvido bug ao enviar mais de um email

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -57,10 +57,9 @@ class Home extends BaseController
             SELECT * FROM clientes
             WHERE DATE_FORMAT(data_nascimento, '%m-%d') 
             BETWEEN DATE_FORMAT('$hoje', '%m-%d') AND DATE_FORMAT('$proximaSemana', '%m-%d')
-        ")->getResult();
+        ")->getResultArray();
 
         if (empty($clientesAniversariantes)) {
-            // Se não há aniversariantes, envie uma mensagem informando
             $msg['msg'] = "Não há aniversariantes na próxima semana!";
 
             return view ('Mysql', $msg);
@@ -70,33 +69,30 @@ class Home extends BaseController
             $email = \Config\Services::email();
     
             foreach ($clientesAniversariantes as $cliente) {
-                $nome = $cliente->nome;
-                $dataNascimento = date('d/m', strtotime($cliente->data_nascimento));
-                $emailCliente = $cliente->email;
-
+                $nome = $cliente['nome'];
+                $dataNascimento = date('d/m', strtotime($cliente['data_nascimento']));
+                $emailCliente = $cliente['email'];
+            
                 $emailsParaParabens[] = $emailCliente;
             
-                // Configurar o email
                 $email->setFrom('juarezribeiro@gmail.com', 'Juarez');
                 $email->setTo($emailCliente);
                 $email->setSubject('Feliz Aniversário!');
-                
-                // Conteúdo da mensagem de parabéns com o nome e data de nascimento do cliente
+            
                 $mensagem = "Olá $nome,\n\nFeliz Aniversário! Desejamos a você um dia maravilhoso e cheio de alegria. Parabéns pelo seu aniversário em $dataNascimento!";
                 $email->setMessage($mensagem);
-                
-                // Enviar o email
+            
                 $email->send();
-
-                $query = $db->query('SELECT * FROM clientes');
-                $result = $query->getResult();
-                $data['result'] = $result;
-                return view('Mysql', [
-                    'result' => $result,
-                    'emailsParaParabens' => $emailsParaParabens,
-                    'data' => $data
-                ]);
             }
+
+            $query = $db->query('SELECT * FROM clientes');
+            $result = $query->getResult();
+            $data['result'] = $result;
+            return view('mysql', [
+                'result' => $result,
+                'emailsParaParabens' => $emailsParaParabens,
+                'data' => $data
+            ]);
         }
      }
 }
